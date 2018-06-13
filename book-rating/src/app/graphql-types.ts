bfb54d5474ecb5dda7006b1c9f3bd57d0218968b
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-06-13 21:42:18 +0200

    hello graphql-code-generator

---
+++ book-rating/src/app/graphql-types.ts
@@ -0,0 +1,58 @@
+/* tslint:disable */
+
+export interface Query {
+  authors?: (Author | null)[] | null;
+  books?: (Book | null)[] | null;
+  book?: Book | null;
+  isbnExists?: boolean | null;
+  bookSearch?: (Book | null)[] | null;
+}
+
+export interface Author {
+  name?: string | null;
+  books?: (Book | null)[] | null;
+}
+
+export interface Book {
+  isbn: string;
+  title?: string | null;
+  subtitle?: string | null;
+  rating?: number | null;
+  description?: string | null;
+  thumbnails?: (Thumbnail | null)[] | null;
+  authors?: (Author | null)[] | null;
+}
+
+export interface Thumbnail {
+  url?: string | null;
+  title?: string | null;
+}
+
+export interface Mutation {
+  createBook?: Book | null;
+}
+
+export interface BookInput {
+  isbn?: string | null;
+  title?: string | null;
+  subtitle?: string | null;
+  rating?: number | null;
+  description?: string | null;
+}
+
+export interface ThumbnailInput {
+  url?: string | null;
+  title?: string | null;
+}
+export interface BookQueryArgs {
+  isbn: string;
+}
+export interface IsbnExistsQueryArgs {
+  isbn: string;
+}
+export interface BookSearchQueryArgs {
+  searchTerm?: string | null;
+}
+export interface CreateBookMutationArgs {
+  book: BookInput;
+}
